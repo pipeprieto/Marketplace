887b022b347091dc1463766d267ea5df5f446e42
ajoshua04 <joshuangarita0413@gmail.com>
2022-06-01 11:53:51 -0500

    cart y cartitem en uno solo

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,10 +4,6 @@ import "./Card.css";
 export default function Card(props) {
   const [cont, setState] = useState(0);
 
-  const addProduct = () => {
-    setState(cont + 1);
-  };
-
   const susbtractProduct = () => {
     setState(cont - 1);
   };
@@ -30,14 +26,7 @@ export default function Card(props) {
               className="btn btn-primary btn-sm flex-fill"
               onClick={susbtractProduct}
             >
-              -
-            </div>
-            <div className="flex-fill text-center">{cont}</div>
-            <div
-              className="btn btn-primary btn-sm flex-fill"
-              onClick={addProduct}
-            >
-              +
+              Agregar al Carrito
             </div>
           </div>
         </div>
--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,5 +1,4 @@
 
-import CardItem from "./CartItem";
 import imagenn from "../../images/prueba.jpeg";
 import styled from "styled-components";
 import { useAlert } from 'react-alert';
@@ -37,14 +36,10 @@ const Cart = () => {
   ]
   );
 
-  useEffect(() => {
-    console.log(items)
-},[items])
 
   const [totalp, setTotal] = useState(0); 
 
   const alert = useAlert();
-
 
 
    const total = (parametro)  => {
@@ -94,16 +89,30 @@ const Cart = () => {
                 <Bottom>
                     <Info onLoad={total}>
                         {items.map((item) => (
-                            <CardItem
-                                tittle = {item.tittle}
-                                imagen = {item.imagen}
-                                alt = 'Error en la imagen'
-                                stars = {item.stars}
-                                price = {item.price}
-                                provider ={item.provider}
-                                bus = {total}
-                                cantidad = {item.cantidad}
-                            />
+                            <Product>
+                            <ProductDetail>
+                              <Image src={item.imagen} alt={item.alt} />
+                              <Details>
+                                <ProductName>
+                                  <b>Product:</b> {item.tittle}
+                                </ProductName>
+                                <ProductStars>
+                                    <b>Stars:</b> {item.stars}
+                                </ProductStars>
+                                <ProductProvider>
+                                <b>Provider:</b> {item.provider}
+                                </ProductProvider>
+                              </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                              <ProductAmountContainer>
+                                <ButtonItem onClick={null}>-</ButtonItem>
+                                <ProductAmount>{item.cantidad}</ProductAmount>
+                                <ButtonItem onClick={null}>+</ButtonItem>
+                              </ProductAmountContainer>
+                              <ProductPrice>$ {item.price}</ProductPrice>
+                            </PriceDetail>
+                          </Product>
                         ))}
                     </Info>
                     <Summary>
@@ -204,4 +213,68 @@ const Button = styled.button`
   color: white;
   font-weight: 600;
   border:0;
+`;
+
+const ButtonItem = styled.button`
+  width: 80px;
+  background-color: #0d6efd;
+  color: white;
+  font-weight: 1000;
+  border: 0;
+`;
+
+const ProductStars = styled.span``;
+
+const ProductProvider = styled.span``;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+  
+`;
+
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+  
 `;
